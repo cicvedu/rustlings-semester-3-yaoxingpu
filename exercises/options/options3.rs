e5025405c0d30ea5bfd6482a3eb40fe885aaaf42
yaoxingpu <yaoxpu@163.com>
2024-03-12 01:43:12 +0800

    update

--- exercises/options/options3.rs
+++ exercises/options/options3.rs
@@ -1,9 +1,17 @@
+/*
+ * @Author: yaoxingpu yaoxpu@163.com
+ * @Date: 2024-03-11 11:52:39
+ * @LastEditors: yaoxingpu yaoxpu@163.com
+ * @LastEditTime: 2024-03-12 01:42:49
+ * @FilePath: /rustlings-semester-3-yaoxingpu/exercises/options/options3.rs
+ * @Description:
+ *
+ */
 // options3.rs
 //
 // Execute `rustlings hint options3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 struct Point {
     x: i32,
@@ -13,7 +21,7 @@ struct Point {
 fn main() {
     let y: Option<Point> = Some(Point { x: 100, y: 200 });
 
-    match y {
+    match &y {
         Some(p) => println!("Co-ordinates are {},{} ", p.x, p.y),
         _ => panic!("no match!"),
     }
