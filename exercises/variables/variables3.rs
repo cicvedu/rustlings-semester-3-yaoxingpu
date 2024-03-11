230038f4fe12f4b291b5b7164acf4f2fed86b6f6
yaoxingpu <yaoxpu@163.com>
2024-03-11 15:38:16 +0800

    update

--- exercises/variables/variables3.rs
+++ exercises/variables/variables3.rs
@@ -1,11 +1,19 @@
+/*
+ * @Author: yaoxingpu yaoxpu@163.com
+ * @Date: 2024-03-11 11:52:39
+ * @LastEditors: yaoxingpu yaoxpu@163.com
+ * @LastEditTime: 2024-03-11 15:14:11
+ * @FilePath: /rustlings-semester-3-yaoxingpu/exercises/variables/variables3.rs
+ * @Description:
+ *
+ */
 // variables3.rs
 //
 // Execute `rustlings hint variables3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let x: i32;
+    let x: i32 = 20;
     println!("Number {}", x);
 }
--- exercises/variables/variables4.rs
+++ exercises/variables/variables4.rs
@@ -1,12 +1,21 @@
+/*
+ * @Author: yaoxingpu yaoxpu@163.com
+ * @Date: 2024-03-11 11:52:39
+ * @LastEditors: yaoxingpu yaoxpu@163.com
+ * @LastEditTime: 2024-03-11 15:34:49
+ * @FilePath: /rustlings-semester-3-yaoxingpu/exercises/variables/variables4.rs
+ * @Description:
+ *
+ */
 // variables4.rs
 //
 // Execute `rustlings hint variables4` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 fn main() {
-    let x = 3;
+    // 变量可变性
+    let mut x = 3;
     println!("Number {}", x);
     x = 5; // don't change this line
     println!("Number {}", x);
